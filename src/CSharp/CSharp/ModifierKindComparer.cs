8a52cbda511552819d29f8b19073dc4fc79d47c8
Coimbra <coimbra.dev@outlook.com>
2022-07-06 03:26:21 -0300

    Change severity of many Analyzers csproj rules

--- src/CSharp/CSharp/ModifierKindComparer.cs
+++ src/CSharp/CSharp/ModifierKindComparer.cs
@@ -19,49 +19,49 @@ namespace Roslynator.CSharp
         {
             switch (kind)
             {
-                case SyntaxKind.NewKeyword:
-                    return 0;
                 case SyntaxKind.PublicKeyword:
-                    return 1;
+                    return 0;
                 case SyntaxKind.PrivateKeyword:
-                    return 2;
+                    return 1;
                 case SyntaxKind.ProtectedKeyword:
-                    return 3;
+                    return 2;
                 case SyntaxKind.InternalKeyword:
+                    return 3;
+                case SyntaxKind.NewKeyword:
                     return 4;
-                case SyntaxKind.ConstKeyword:
-                    return 5;
-                case SyntaxKind.StaticKeyword:
-                    return 6;
-                case SyntaxKind.VirtualKeyword:
-                    return 7;
-                case SyntaxKind.SealedKeyword:
-                    return 8;
-                case SyntaxKind.OverrideKeyword:
-                    return 9;
-                case SyntaxKind.AbstractKeyword:
-                    return 10;
-                case SyntaxKind.ReadOnlyKeyword:
-                    return 11;
-                case SyntaxKind.ExternKeyword:
-                    return 12;
                 case SyntaxKind.UnsafeKeyword:
-                    return 13;
-                case SyntaxKind.FixedKeyword:
-                    return 14;
+                    return 5;
+                case SyntaxKind.ConstKeyword:
+                    return 6;
+                case SyntaxKind.StaticKeyword:
+                    return 7;
+                case SyntaxKind.ReadOnlyKeyword:
+                    return 8;
                 case SyntaxKind.VolatileKeyword:
-                    return 15;
+                    return 9;
+                case SyntaxKind.ExternKeyword:
+                    return 10;
+                case SyntaxKind.AbstractKeyword:
+                    return 11;
+                case SyntaxKind.VirtualKeyword:
+                    return 12;
+                case SyntaxKind.SealedKeyword:
+                    return 13;
+                case SyntaxKind.OverrideKeyword:
+                    return 14;
                 case SyntaxKind.AsyncKeyword:
+                    return 15;
+                case SyntaxKind.FixedKeyword:
                     return 16;
                 case SyntaxKind.PartialKeyword:
                     return 17;
-                case SyntaxKind.ThisKeyword:
-                    return 18;
-                case SyntaxKind.RefKeyword:
-                    return 19;
-                case SyntaxKind.OutKeyword:
-                    return 20;
                 case SyntaxKind.InKeyword:
+                    return 18;
+                case SyntaxKind.OutKeyword:
+                    return 19;
+                case SyntaxKind.RefKeyword:
+                    return 20;
+                case SyntaxKind.ThisKeyword:
                     return 21;
                 default:
                     {
